f7bd939e64f9e16d3de6b672d556079166ff5232
SaulAlamilla <l18390023@chetumal.tecnm.mx>
2022-11-16 12:03:10 -0500

    Vinculos con vista de botones

--- resources/views/capturar_resultados/index.blade.php
+++ resources/views/capturar_resultados/index.blade.php
@@ -14,19 +14,19 @@
                                     @csrf
                                     @method('patch')
                                     <div x-data="{view: true, edit: false}" class="flex flex-col md:flex-row items-center mb-6">
-                                        <div class="basis-1/4 mr-4">
+                                        <div class="basis-2/4 mr-4">
                                             <label for="{{ $concepto->concepto }}">{{ $concepto->concepto }}</label>
                                         </div>
-                                        <textarea name="conclusion" rows="4" id="{{ $concepto->concepto }}" x-bind:disabled="view"
+                                        <textarea name="conclusion" placeholder="Agregar conclusión" rows="4" id="{{ $concepto->concepto }}" x-bind:disabled="view"
                                             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">{{
-                                                $concepto->conclusion == "" ? "Agregar conclusion ": $concepto->conclusion
+                                                $concepto->conclusion == "" ? "": $concepto->conclusion
                                             }}</textarea>
                                         <div class="mx-4" x-show="view">
-                                            <a @click=" view= false, edit= true">editar</a>
+                                            <a @click=" view= false, edit= true"  class="text-white text-base bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-3 py-2.5 text-center mr-2 md:mr-0">Editar</a>
                                         </div>
                                         <div class="mx-4" x-show="edit">
-                                            <button>Guardar</button>
-                                            <a @click=" view= true, edit= false">cancelar</a>
+                                            <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-base px-4 py-2.5 my-1 text-center">Guardar</button>
+                                            <a @click=" view= true, edit= false"  class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-base px-4 py-2.5 my-1 text-center">Cancelar</a>
                                         </div>
                                     </div>
                                     <hr class="my-8 h-px bg-gray-700 border-0">
